160472669aaf3f789ab723ef15ac73f25f33013c
Laura Tozer <ljctozer@gmail.com>
2022-02-22 22:35:26 +0000

    First commit

---
+++ Vowels in a string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VowelsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CountVowels(string localString)
+            {
+                int count = 0;
+                Regex reg = new Regex("[aeiou]");
+                for (int i = 0; i < localString.Length; i++)
+                {
+                    string letter = localString[i].ToString();
+                    if (reg.IsMatch(letter))
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            Console.WriteLine(CountVowels("aeiou"));
+        }
+    }
+}
